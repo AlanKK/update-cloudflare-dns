2f6bd24545c8c0d2f245879f9ecf861b9e9a8e87
Alan Keister <alankeister@gmail.com>
2024-08-26 19:41:22 -0400

    Add pushbullet notifications, -c <config.file>

--- Makefile
+++ Makefile
@@ -5,13 +5,13 @@ BINARY_NAME=ip_update
 all: darwin linux windows
 
 darwin:
-	GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o $(BINARY_NAME) main.go
+	GOOS=$(GOOS) GOARCH=$(GOARCH) go build -o $(BINARY_NAME) main.go pushbullet.go
 
 linux:
-	GOOS=linux GOARCH=$(GOARCH) go build -o $(BINARY_NAME).linux main.go
+	GOOS=linux GOARCH=$(GOARCH) go build -o $(BINARY_NAME).linux main.go pushbullet.go
 
 windows:
-	GOOS=windows GOARCH=$(GOARCH) go build -o $(BINARY_NAME).exe main.go
+	GOOS=windows GOARCH=$(GOARCH) go build -o $(BINARY_NAME).exe main.go pushbullet.go
 
 clean:
 	rm -f $(BINARY_NAME) $(BINARY_NAME).linux $(BINARY_NAME).exe
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"flag"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,12 +13,13 @@ import (
 	"time"
 )
 
-type Config struct {
-	API          string `json:"api"`
+type IPChangeConfig struct {
+	PushApiKey   string `json:"pushbullet-api-key"`
+	CfAPI_Key    string `json:"cf-api-key"`
 	UpdateTarget []struct {
 		Name   string `json:"name"`
 		ID     string `json:"id"`
-		ZoneID string `json:"zone_id"`
+		ZoneID string `json:"zone-id"`
 	} `json:"update-target"`
 }
 
@@ -37,25 +39,20 @@ type UpdateResult struct {
 	Success bool `json:"success"`
 }
 
-func logMessage(message string) {
-	timestamp := time.Now().Format("%Y-%m-%d %H:%M:%S")
-	fmt.Printf("%s %s\n", timestamp, message)
-}
-
 func getCurrentIP() (string, error) {
 	resp, err := http.Get("http://checkip.amazonaws.com")
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 	defer resp.Body.Close()
 	body, err := ioutil.ReadAll(resp.Body)
 	if err != nil {
-		return "", err
+		return "", nil
 	}
 	return strings.TrimSpace(string(body)), nil
 }
 
-func getDNSStatus(apiKey, zoneID, recordID string) (*DNSRecord, error) {
+func getCurrentDNSEntry(apiKey string, zoneID string, recordID string) (*DNSRecord, error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zoneID, recordID), nil)
 	if err != nil {
 		return nil, err
@@ -75,7 +72,7 @@ func getDNSStatus(apiKey, zoneID, recordID string) (*DNSRecord, error) {
 	return &dnsRecord, nil
 }
 
-func updateDNSRecord(apiKey, zoneID, recordID, name, content string, ttl int, proxied bool) (*UpdateResult, error) {
+func updateDNSRecord(apiKey string, zoneID string, recordID string, name string, content string, ttl int, proxied bool) (*UpdateResult, error) {
 	data := fmt.Sprintf(`{"type":"A","name":"%s","content":"%s","ttl":%d,"proxied":%t}`, name, content, ttl, proxied)
 	req, err := http.NewRequest("PUT", fmt.Sprintf("https://api.cloudflare.com/client/v4/zones/%s/dns_records/%s", zoneID, recordID), bytes.NewBufferString(data))
 	if err != nil {
@@ -96,36 +93,73 @@ func updateDNSRecord(apiKey, zoneID, recordID, name, content string, ttl int, pr
 	return &updateResult, nil
 }
 
+func getArgs() string {
+	if len(os.Args) != 3 {
+		fmt.Println("Usage:", os.Args[0], "<title> <body>")
+		os.Exit(1)
+	}
+
+	var configPath string
+	flag.StringVar(&configPath, "c", "", "Path to config file")
+	flag.Parse()
+
+	if configPath != "" {
+		if _, err := os.Stat(configPath); os.IsNotExist(err) {
+			log.Fatalf("Config file '%s' does not exist", configPath)
+		}
+	}
+	return configPath
+}
+
 func main() {
-	configFile, err := os.Open("config.json")
+	configPath := getArgs()
+
+	configFile, err := os.Open(configPath)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer configFile.Close()
-	var config Config
+
+	var config IPChangeConfig
 	err = json.NewDecoder(configFile).Decode(&config)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	currentIP, err := getCurrentIP()
-	if err != nil {
-		logMessage("Check your internet connection")
-		return
+	maxRetries := 120 // 10 mins
+	retries := 0
+	var currentIP string
+
+	// Keep trying until we can get a connection and result from the internet
+	for {
+		currentIP, err = getCurrentIP()
+		if currentIP == "" || err != nil {
+			log.Printf("No internet connection. Retrying in 5 seconds...")
+			time.Sleep(5 * time.Second)
+			retries++
+			if retries >= maxRetries {
+				log.Printf("Failed to get current IP after max retries")
+				return
+			}
+			continue
+		}
+		break
 	}
 
+	// Change the cloudflare dns record
 	for _, target := range config.UpdateTarget {
-		dnsRecord, err := getDNSStatus(config.API, target.ZoneID, target.ID)
+		dnsRecord, err := getCurrentDNSEntry(config.CfAPI_Key, target.ZoneID, target.ID)
 		if err != nil {
-			logMessage("Error Occurred While Accessing Current DNS Status. May Caused by outdated config file. Please re-generate config.json file (run configure.bash)")
+			log.Printf("Error Occurred While Accessing Current DNS Status. May Caused by outdated config file. Please re-generate config.json file (run configure.bash)")
 			return
 		}
 		if dnsRecord.Result.Content != currentIP {
-			_, err := updateDNSRecord(config.API, target.ZoneID, target.ID, target.Name, currentIP, dnsRecord.Result.TTL, dnsRecord.Result.Proxied)
+			_, err := updateDNSRecord(config.CfAPI_Key, target.ZoneID, target.ID, target.Name, currentIP, dnsRecord.Result.TTL, dnsRecord.Result.Proxied)
 			if err != nil {
-				logMessage("Error While updating " + target.Name)
+				log.Printf("Error While updating " + target.Name)
 			} else {
-				logMessage(target.Name + ": successfully updated from " + dnsRecord.Result.Content + " to " + currentIP)
+				log.Printf(target.Name + ": successfully updated from " + dnsRecord.Result.Content + " to " + currentIP)
+				sendPushbullet(config.PushApiKey, "EC2 Instance Ready", currentIP)
 			}
 		}
 	}
